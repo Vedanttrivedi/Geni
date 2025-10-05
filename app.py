2264953464a93049b255c325abdd84b76f95ef6c
trivedivedant2002 <trivedivedant2002@gmail.com>
2025-10-05 11:00:24 +0530

    added route with ai service

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import boto3
+from datetime import datetime
 #from boto3.exceptions import ClientError
 
 def get_client(service_name:str, region:str):
@@ -6,24 +7,58 @@ def get_client(service_name:str, region:str):
 
     return client
 
+
 model_id = "meta.llama3-8b-instruct-v1:0"
 service_name = "bedrock-runtime"
 region = "us-east-1"
 
+
+
+def generate_details(client,topic):
+    query = f" {topic}"
+    conversation = [
+        {
+            "role": "user",
+            "content": [{"text": query}],
+        }
+    ]
+    return  f"Information about {topic}"
+    try:
+        response = client.converse(modelId=model_id, messages=conversation,inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9})
+        response_text = response["output"]["message"]["content"][0]["text"]
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+    return response_text
+
+def save_data(s3_key,s3_bucket, payload):
+    s3 = boto3.client('s3')
+
+    try:
+        s3.put_object(Bucket = s3_bucket, Key = s3_key, Body = payload)
+        print("Saved Code")
+    except Exception as e:
+        print(f"Something went wrong {e}")
+    
 client = get_client(service_name, region)
-query = "TEll me world 7 wonders"
 
-conversation = [
-    {
-        "role": "user",
-        "content": [{"text": query}],
-    }
-]
+def lambda_handler(event,context):
+    topic = event["body"]
+    global client
+    data = generate_details(client,topic)
+    if data is None:
+        print("Something went wrong ! Could not fetch data")
+        return None
+    
+    print(f"Data is :::{data} ")
+    current_time = datetime.now().strftime('%H%M%S')
+    s3_key = f"outputs/{current_time}.txt"
+    s3_bucket = "geni-bedrock"
+    save_data(s3_key=s3_key,s3_bucket=s3_bucket,payload=data)
 
-try:
-    response = client.converse(modelId=model_id, messages=conversation,inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9})
-    response_text = response["output"]["message"]["content"][0]["text"]
-    print(response_text)
 
-except Exception as e:
-    print(f"Error: {e}")
+    
+
+lambda_handler({"body":"indian cricket team"},None)
